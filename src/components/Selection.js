e388ca7b8ca1be18ea2f28b444d1b10077f2a928
Anil Kumar <anilkumar@Anils-MacBook-Air.local>
2025-07-12 17:01:29 +0530

    Completed assignment

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,10 @@
+import React, { useState } from "react";
+const Selection = ({ applyColor }) => {
+  const [bgStyle, setBgStyle] = useState({ background: "" });
+  const handleClick = () => {
+    applyColor(setBgStyle);
+  };
+  return <div className="fix-box" style={bgStyle} onClick={handleClick}></div>;
+};
 
+export default Selection;
